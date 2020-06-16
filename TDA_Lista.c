0f831b3a854bf380a998a274b7d64f1f59fc79f4
Skreignether <66978628+Skreignether@users.noreply.github.com>
2020-06-15 20:48:44 -0400

    Update TDA_Lista.c

--- TDA_Lista.c
+++ TDA_Lista.c
@@ -1,14 +1,20 @@
 //En c
+//Se usó Recuperar(p,Lista) y Fin(Lista), tomando en cuenta que Fin(Lista) entrega la posición n+1, 
+//siendo n+1 el Largo de la Lista (Si hay 3 elementos en una lista, la ultima posición [n] es 2, ya que [ 0, 1, 2], entonces n+1 es 3)
 
 float promedio(Lista *L){
-  int auxiliar=0;
-  int i,suma;
+  int auxiliar;
+  int i;
+  float suma=0;
+  float promedio;
   
   for(i=0; i<Fin(L);i++{
     auxiliar= Recuperar(i, L);
     suma= suma+auxiliar;
   }
   
-  return (suma/Fin(L));
+  promedio= (suma/Fin(L));
+      
+  return promedio;
 
 }
